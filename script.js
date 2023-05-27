3be6b0ca178ecd8d2bf42ba4a363589a7b7774b9
Sahal Dahir <sahaldahir@Sahals-MacBook-Pro.local>
2023-05-27 21:28:54 +0300

    this is my grade-Assessment-

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello");
+const num1 = 96;
+const num2 = 83;
+const mum3 = 70;
+const num4 = 59;
+
+if ("if num <100  >90") {
+  console.log("you got grade A");
+} else "if num  <90 >75";
+{
+  console.log("you got grade B");
+}
+if ("if num <75 >60") {
+  console.log("you got grade C");
+} else "if num <60 ";
+{
+  console.log("you got grade D");
+}
